f953818bad22917798475839e758284bf025b29c
Bartosz Alchimowicz <bartosz.alchimowicz@cs.put.poznan.pl>
2012-03-02 17:51:29 +0100

    fix link

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
 	version = '0.0.1',
 	author = read("AUTHORS"),
 	keywords = "regular expressions generator",
-	url = "https://github.com/perfidia/seleshot",
+	url = "https://github.com/perfidia/regexpgen",
 	package_dir = {'': PACKAGE_DIR},
 	long_description = read("README.md"),
 	packages = find_packages(PACKAGE_DIR, exclude=['ez_setup', 'examples', 'tests']),
