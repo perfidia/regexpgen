61717eea68297f1a70a9875b554b78c69c00b309
Michzimny <michzimny@michzimny.pl>
2012-03-12 18:13:03 +0100

    matching start and end flag (^..$)

--- src/regexpgen/__init__.py
+++ src/regexpgen/__init__.py
@@ -13,20 +13,27 @@ max - wartość maksymalna, domyślna wartość: None
 
 import minteger, mreal, mdate, mtime, mdatetime, mnnint
 
-def nnint(format, min = None, max = None):
-	return mnnint.run(format, min, max)
+def nnint(format, min = None, max = None, matchStartEnd = True):
+	return startEndMatcher(mnnint.run(format, min, max), matchStartEnd)
 
-def integer(format, min = None, max = None):
-	return minteger.run(format, min, max)
+def integer(format, min = None, max = None, matchStartEnd = True):
+	return startEndMatcher(minteger.run(format, min, max), matchStartEnd)
 
-def real(format, min = None, max = None):
-	return mreal.run(format, min, max)
+def real(format, min = None, max = None, matchStartEnd = True):
+	return startEndMatcher(mreal.run(format, min, max), matchStartEnd)
 
-def date(format, min = None, max = None, timezone = None):
-	return mdate.run(format, min, max, timezone)
+def date(format, min = None, max = None, timezone = None, matchStartEnd = True):
+	return startEndMatcher(mdate.run(format, min, max, timezone), matchStartEnd)
 
-def time(format, min = None, max = None, timezone = None):
-	return mtime.run(format, min, max, timezone)
+def time(format, min = None, max = None, timezone = None, matchStartEnd = True):
+	return startEndMatcher(mtime.run(format, min, max, timezone), matchStartEnd)
 
-def datetime(format, date_min = None, date_max = None, time_min = None, time_max = None, timezone = None):
-	return mdatetime.run(format, date_min, date_max, time_min, time_max, timezone)
+def datetime(format, date_min = None, date_max = None, time_min = None, time_max = None, timezone = None, matchStartEnd = True):
+	return startEndMatcher(mdatetime.run(format, date_min, date_max, time_min, time_max, timezone), matchStartEnd)
+
+
+def startEndMatcher(re_str, matchStartEnd = True):
+	if(not matchStartEnd):
+		re_str = re_str.replace("^",'')
+		re_str = re_str.replace("$",'')
+	return re_str
