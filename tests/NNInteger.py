15758e29afc0f038a74dd1de4e1f940cff8b6119
Venomo <venomo@tlen.pl>
2013-03-03 19:17:11 +0100

    Tests rewritten.

--- tests/NNInteger.py
+++ tests/NNInteger.py
@@ -6,6 +6,8 @@ Created on Mar 16, 2012
 import unittest
 import regexpgen
 import re
+import random
+import math
 
 class Test(unittest.TestCase):
 	def testStartEnd(self):
@@ -19,20 +21,97 @@ class Test(unittest.TestCase):
 		self.assertNotEqual(regexpgen.nnint("%d", matchStartEnd = False)[-1], '$')
 
 	def testDefault(self):
-		regexp = regexpgen.nnint("%d")
-		print regexp
+		random.seed(0);
 
-		for i in [ i for i in xrange(0, 65000) ]:
-			print "check", i
-			self.assertNotEqual(re.match(regexp, str(i)), None)
+		for i in xrange(0, 10000):
+			scale = len(str(i))
+			self.__runTest1__(scale, False, False);
+			self.__runTest1__(scale, False, True);
+			self.__runTest1__(scale, True, False);
+			self.__runTest1__(scale, True, True);
+			self.__runTest2__(scale, False, False);
+			self.__runTest2__(scale, False, True);
+			self.__runTest2__(scale, True, False);
+			self.__runTest2__(scale, True, True);
+			self.__runTest3__(scale, False, False);
+			self.__runTest3__(scale, False, True);
+			self.__runTest3__(scale, True, False);
+			self.__runTest3__(scale, True, True);
 
-		for i in [ i for i in xrange(-65000, -1 + 1) ]:
-			print "check", i
-			self.assertEqual(re.match(regexp, str(i)), None)
 
-		for i in [ 'a', 1.1, -0.8 ]:
-			print "check", i
-			self.assertEqual(re.match(regexp, str(i)), None)
+	def __runTest1__(self, scale, setMin, setMax):
+		min = random.randint(0, 10**scale) if setMin else 0
+		max = random.randint(min, min + 10**scale)
+		format = "%d"
+		regexp = regexpgen.nnint(format, min if setMin else None, max if setMax else None)
+		for i in xrange(-max * 2, -1): #, (max + 100)/100):
+			self.assertFalse(re.match(regexp, str(i)), self.__getInfo__(str(i), regexp, format, min, max))
+		for i in xrange(0, min - 1):
+			self.assertFalse(re.match(regexp, str(i)), self.__getInfo__(str(i), regexp, format, min, max))
+		for i in xrange(min, max):
+			a = "0" + str(i); b = "00" + str(i); c = "000" + str(i); d = "0000" + str(i);
+			self.assertTrue(re.match(regexp, str(i)), self.__getInfo__(i, regexp, format, min, max))
+			self.assertTrue(re.match(regexp, a), self.__getInfo__(a, regexp, format, min, max))
+			self.assertTrue(re.match(regexp, b), self.__getInfo__(b, regexp, format, min, max))
+			self.assertTrue(re.match(regexp, c), self.__getInfo__(c, regexp, format, min, max))
+			self.assertTrue(re.match(regexp, d), self.__getInfo__(d, regexp, format, min, max))
+		if (setMax):
+			for i in xrange(max+1, max * 2):
+				self.assertFalse(re.match(regexp, str(i)), self.__getInfo__(str(i), regexp, format, min, max))
+
+	def __runTest2__(self, scale, setMin, setMax):
+		min = random.randint(0, 10**scale) if setMin else 0
+		max = random.randint(min, min + 10**scale)
+		format = "%0d"
+		regexp = regexpgen.nnint(format, min if setMin else None, max if setMax else None)
+		for i in xrange(-max * 2, -1):#, (max + 100)/100):
+			self.assertFalse(re.match(regexp, str(i)), self.__getInfo__(str(i), regexp, format, min, max))
+		for i in xrange(0, min - 1):
+			self.assertFalse(re.match(regexp, str(i)), self.__getInfo__(str(i), regexp, format, min, max))
+		for i in xrange(min, max):
+			a = "0" + str(i); b = "00" + str(i); c = "000" + str(i); d = "0000" + str(i);
+			self.assertTrue(re.match(regexp, str(i)), self.__getInfo__(i, regexp, format, min, max))
+			self.assertFalse(re.match(regexp, a), self.__getInfo__(a, regexp, format, min, max))
+			self.assertFalse(re.match(regexp, b), self.__getInfo__(b, regexp, format, min, max))
+			self.assertFalse(re.match(regexp, c), self.__getInfo__(c, regexp, format, min, max))
+			self.assertFalse(re.match(regexp, d), self.__getInfo__(d, regexp, format, min, max))
+		if (setMax):
+			for i in xrange(max+1, max * 2):
+				self.assertFalse(re.match(regexp, str(i)), self.__getInfo__(str(i), regexp, format, min, max))
+
+	def __runTest3__(self, scale, setMin, setMax):
+		getZeros = lambda x: (scale - len(str(x)))*"0"
+		min = random.randint(0, 10**scale) if setMin else 0
+		max = random.randint(min, min + 10**scale)
+		format = "%0{0}d".format(scale)
+		regexp = regexpgen.nnint(format, min if setMin else None, max if setMax else None)
+		for i in xrange(-max * 2, -1):#, (max + 100)/100):
+			a = "-" + getZeros(i) + str(math.fabs(i));
+			self.assertFalse(re.match(regexp, str(i)), self.__getInfo__(str(i), regexp, format, min, max))
+			self.assertFalse(re.match(regexp, a), self.__getInfo__(a, regexp, format, min, max))
+		for i in xrange(0, min - 1):
+			a = getZeros(i) + str(i);
+			self.assertFalse(re.match(regexp, str(i)), self.__getInfo__(str(i), regexp, format, min, max))
+			self.assertFalse(re.match(regexp, a), self.__getInfo__(a, regexp, format, min, max))
+		for i in xrange(min, max):
+			zeros = getZeros(i)
+			a = zeros + str(i); b = zeros + "0" + str(i);
+			self.assertTrue(re.match(regexp, a), self.__getInfo__(a, regexp, format, min, max))
+			self.assertFalse(re.match(regexp, b), self.__getInfo__(b, regexp, format, min, max))
+			if (len(zeros) > 0):
+				c = zeros[:-1] + str(i) #cuts last zero
+				self.assertFalse(re.match(regexp, c), self.__getInfo__(c, regexp, format, min, max))
+		if (setMax):
+			for i in xrange(max+1, max * 2):
+				a = getZeros(i) + str(i)
+				self.assertFalse(re.match(regexp, str(i)), self.__getInfo__(str(i), regexp, format, min, max))
+				self.assertFalse(re.match(regexp, a), self.__getInfo__(a, regexp, format, min, max))
+
+
+
+	def __getInfo__(self, i, regexp, format, min, max):
+		return "Failed! Number: {0}, min: {1}, max: {2}, format: {3}, regexp: {4}".format(i, str(min), str(max), format, regexp)
+
 
 if __name__ == "__main__":
 	#import sys;sys.argv = ['', 'Test.testName']
