97fb8430fb709ed2bdf70afc5233edf793fa7764
Venomo <venomo@tlen.pl>
2013-03-24 18:12:28 +0100

    0.Ylf tests

--- tests/Real.py
+++ tests/Real.py
@@ -25,6 +25,10 @@ class Test(unittest.TestCase):
 			self.__runTest2(scale, False, True);
 			self.__runTest2(scale, True, False);
 			self.__runTest2(scale, True, True);
+			self.__runTest3(scale, False, False);
+			self.__runTest3(scale, False, True);
+			self.__runTest3(scale, True, False);
+			self.__runTest3(scale, True, True);
 			print i
 
 	def __runTest1(self, scale, setMin, setMax):
@@ -35,24 +39,7 @@ class Test(unittest.TestCase):
 		regexp = regexpgen.real(format, min if setMin else None, max if setMax else None)
 		info = lambda value: self.__getInfo(value, regexp, format, min if setMin else None, max if setMax else None)
 
-		if min < 0 and max < 0:
-			rangeLeft = min*2
-			rangeRight = -min*2
-		if min < 0 and max > 0:
-			rangeLeft = min*2
-			rangeRight = max*2
-		if min > 0 and max > 0:
-			rangeLeft = -max*2
-			rangeRight = max*2
-		if min == 0 and max != 0:
-			rangeLeft = -max*2
-			rangeRight = max*2
-		if max == 0 and min != 0:
-			rangeLeft = min*2
-			rangeRight = -min*2
-		if max == 0 and min == 0:
-			rangeLeft = -100
-			rangeRight = 100
+		(rangeLeft, rangeRight) = self.__getRanges(min, max)
 
 		rest = min - int(min)
 		rest = math.fabs(rest)
@@ -84,24 +71,7 @@ class Test(unittest.TestCase):
 		regexp = regexpgen.real(format, min if setMin else None, max if setMax else None)
 		info = lambda value: self.__getInfo(value, regexp, format, min if setMin else None, max if setMax else None)
 
-		if min < 0 and max < 0:
-			rangeLeft = min*2
-			rangeRight = -min*2
-		if min < 0 and max > 0:
-			rangeLeft = min*2
-			rangeRight = max*2
-		if min > 0 and max > 0:
-			rangeLeft = -max*2
-			rangeRight = max*2
-		if min == 0 and max != 0:
-			rangeLeft = -max*2
-			rangeRight = max*2
-		if max == 0 and min != 0:
-			rangeLeft = min*2
-			rangeRight = -min*2
-		if max == 0 and min == 0:
-			rangeLeft = -100
-			rangeRight = 100
+		(rangeLeft, rangeRight) = self.__getRanges(min, max)
 
  		rest = min - int(min)
 		rest = math.fabs(rest)
@@ -125,6 +95,90 @@ class Test(unittest.TestCase):
 			while i <= rangeRight:
 				self.assertFalse(re.search(regexp, str(i)), info(str(i)))
 				i = i + 1.05
+
+	def __runTest3(self, scale, setMin, setMax):
+		min = random.uniform(-1*(10**scale), 10**scale)
+		max = random.uniform(min, min + 5**scale)
+		format = "%0.{0}lf".format(scale)
+		regexp = regexpgen.real(format, min if setMin else None, max if setMax else None)
+		info = lambda value: self.__getInfo(value, regexp, format, min if setMin else None, max if setMax else None)
+
+		(rangeLeft, rangeRight) = self.__getRanges(min, max)
+
+ 		rest = min - int(min)
+		rest = math.fabs(rest)
+		i = rangeLeft
+		if setMin:
+			while i < min :
+				x = self.__sliceFloat(i, scale - 1 if scale > 0 else 0)
+				y = self.__sliceFloat(i, scale)
+				z = self.__sliceFloat(i, scale + 1)
+				self.assertFalse(re.match(regexp, str(x)), info(str(x)))
+				self.assertFalse(re.match(regexp, str(y)), info(str(y)))
+				self.assertFalse(re.match(regexp, str(z)), info(str(z)))
+			 	i = i + 1.05
+		while i <= max:
+			if i >= 0:
+				a = "0" + str(i); b = "00" + str(i); c = "000" + str(i); d = "0000" + str(i);
+			else:
+				a = "-0" + str(-i); b = "-00" + str(-i); c = "-000" + str(-i); d = "-0000" + str(-i);
+
+			x = self.__sliceFloat(i, scale - 1 if scale > 0 else 0)
+			y = self.__sliceFloat(i, scale)
+			z = self.__sliceFloat(i, scale + 1)
+
+			self.assertTrue(re.match(regexp, str(y)), info(str(y)))
+			if (x != y):
+				self.assertFalse(re.match(regexp, str(x)), info(str(x)))
+
+			self.assertFalse(re.match(regexp, str(z)), info(str(z)))
+			self.assertFalse(re.match(regexp, a), info(a))
+			self.assertFalse(re.match(regexp, b), info(b))
+			self.assertFalse(re.match(regexp, c), info(c))
+			self.assertFalse(re.match(regexp, d), info(d))
+			i = i + 1.05
+		if setMax:
+			while i <= rangeRight:
+				x = self.__sliceFloat(i, scale - 1 if scale > 0 else 0)
+				y = self.__sliceFloat(i, scale)
+				z = self.__sliceFloat(i, scale + 1)
+				self.assertFalse(re.match(regexp, str(x)), info(str(x)))
+				self.assertFalse(re.match(regexp, str(y)), info(str(y)))
+				self.assertFalse(re.match(regexp, str(z)), info(str(z)))
+				i = i + 1.05
+
+	def __sliceFloat(self, f, scale):
+		splitted = str(f).split(".")
+
+		if (len(splitted[1]) > scale):
+			x = splitted[1][0:scale]
+		elif (len(splitted[1]) < scale):
+			x = splitted[1] + "0"*(scale -len(splitted[1]))
+		else:
+			x = splitted[1]
+
+		return float(splitted[0] + "." + x)
+
+	def __getRanges(self, min, max):
+		if min < 0 and max < 0:
+			rangeLeft = min*2
+			rangeRight = -min*2
+		if min < 0 and max > 0:
+			rangeLeft = min*2
+			rangeRight = max*2
+		if min > 0 and max > 0:
+			rangeLeft = -max*2
+			rangeRight = max*2
+		if min == 0 and max != 0:
+			rangeLeft = -max*2
+			rangeRight = max*2
+		if max == 0 and min != 0:
+			rangeLeft = min*2
+			rangeRight = -min*2
+		if max == 0 and min == 0:
+			rangeLeft = -100
+			rangeRight = 100
+		return (rangeLeft, rangeRight)
 
 	def __getInfo(self, i, regexp, format, min, max):
 		return "Failed! Number: {0}, min: {1}, max: {2}, format: {3}, regexp: {4}".format(i, str(min), str(max), format, regexp)
