d4630fa7128c7ffb7faf708e6089b3fc0e40bfb1
Bartosz Alchimowicz <bartosz.alchimowicz@cs.put.poznan.pl>
2013-04-19 02:15:33 +0200

    add classifiers

--- setup.py
+++ setup.py
@@ -8,12 +8,22 @@ def read(file_name):
 
 setup(
 	name = "regexpgen",
-	version = '0.0.1',
+	version = '0.0.2',
 	author = read("AUTHORS"),
-	keywords = "regular expressions generator",
+	keywords = "regular expression generator",
 	url = "https://github.com/perfidia/regexpgen",
 	package_dir = {'': PACKAGE_DIR},
 	long_description = read("README.md"),
 	packages = find_packages(PACKAGE_DIR, exclude=['ez_setup', 'examples', 'tests']),
 	zip_safe = False,
+	# https://pypi.python.org/pypi?%3aaction=list_classifiers
+	classifiers = [
+			'Environment :: Console',
+			'Environment :: Web Environment',
+			'Intended Audience :: Developers',
+			'License :: OSI Approved :: MIT License',
+			'Operating System :: Microsoft :: Windows',
+			'Operating System :: POSIX',
+			'Programming Language :: Python :: 2.7',
+	],
 )
